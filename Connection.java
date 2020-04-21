326cdf18e820a2f0992f6c9bd0814dac0813e7eb
AttraPrasanna <63235228+AttraPrasanna@users.noreply.github.com>
2020-04-21 13:33:22 +0530

    added seperate files for java development

---
+++ Connection.java
@@ -0,0 +1,63 @@
+package com.java;
+
+import java.awt.HeadlessException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Connection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		java.sql.Connection conn = null;
+		PreparedStatement pst = null;
+		ResultSet rs= null;
+		
+		try {
+//			System.out.println("1");
+			
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			
+//			System.out.println("2");
+			
+			conn = DriverManager.getConnection("jdbc:ucanaccess://D:\\mydb.accdb");
+			
+//			System.out.println("3");
+			
+			String sql = "select * from account";
+			
+//			System.out.println("4");
+			
+			pst = conn.prepareStatement(sql);
+			
+//			System.out.println("5");
+			
+			rs = pst.executeQuery();
+			
+//			System.out.println("6");
+			
+			while(rs.next()) {
+				
+//				System.out.println("Conncetion sucessful");
+				System.out.println("\n"+rs.getString("CustName")+ "\t" + 
+										rs.getString("CustAccountNo")+"\t" + 
+										rs.getString("Cust_A_Type")+"\t" +	
+										rs.getString("Cust_Curr")+"\t"+
+										rs.getString("Amount"));
+			}
+
+			
+		}catch(HeadlessException | ClassNotFoundException | SQLException e) {
+			
+//			System.out.println("Hello you are in Catch");
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+			System.out.println(e.getStackTrace());
+			System.out.println(e.fillInStackTrace());
+		}
+
+	}
+
+}
